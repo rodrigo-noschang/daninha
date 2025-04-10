8da16bb1b07ce9cc91c9e35364a5234fdba1e4da
Rodrigo <rodrigo.noschang1@gmail.com>
2025-04-09 23:38:07 -0300

    initial configuration and first components... elsint and prettier configured

---
+++ src/app/components/PageHeader.tsx
@@ -0,0 +1,11 @@
+export function PageHeader() {
+  return (
+    <div className="text-xl text-center">
+      Organize a sua...
+      <span className="ml-2 bg-gradient-to-r from-red-600 via-green-500 to-purple-400 text-transparent bg-clip-text text-2xl">
+        FODINHA
+      </span>
+      <div className="mt-2">😩😩😩</div>
+    </div>
+  );
+}
